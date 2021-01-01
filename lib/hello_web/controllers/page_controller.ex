c97f14427de34e4cb0eacb9282dd37f99a09a6c8
Konstantinos <kkasidiaris@gmail.com>
2021-01-01 02:14:22 +0200

    fool around with views

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -1,28 +1,36 @@
 defmodule HelloWeb.PageController do
   use HelloWeb, :controller
 
-  def index(conn, _params) do
-    # render(conn, :index)
+  # def index(conn, _params) do
+  #   # render(conn, :index)
 
-    # conn
-    # |> put_layout("admin.html")
-    # |> render("index.html")
+  #   # conn
+  #   # |> put_layout("admin.html")
+  #   # |> render("index.html")
 
-    # conn
-    # |> put_status(202)
-    # |> render("index.html")
+  #   # conn
+  #   # |> put_status(202)
+  #   # |> render("index.html")
 
-    conn
-    |> put_flash(:info, "Welcome to Phoenix, from flash info!")
-    |> put_flash(:error, "Let's pretend there is an error")
-    |> redirect(to: Routes.page_path(conn, :redirect_test))
-  end
+  #   conn
+  #   |> put_flash(:info, "Welcome to Phoenix, from flash info!")
+  #   |> put_flash(:error, "Let's pretend there is an error")
+  #   |> redirect(to: Routes.page_path(conn, :redirect_test))
+  # end
 
   def redirect_test(conn, _params) do
     render(conn, "index.html")
   end
 
-  def show(conn, %{"messenger" => messenger}) do
-    json(conn, %{id: messenger})
+  def show(conn, _params) do
+    page = %{title: "foo"}
+
+    render(conn, "show.json", page: page)
+  end
+
+  def index(conn, _params) do
+    pages = [%{title: "foo"}, %{title: "bar"}]
+
+    render(conn, "index.json", pages: pages)
   end
 end
--- lib/hello_web/views/layout_view.ex
+++ lib/hello_web/views/layout_view.ex
@@ -1,3 +1,11 @@
 defmodule HelloWeb.LayoutView do
   use HelloWeb, :view
+
+  def title() do
+    "Awesome new title"
+  end
+
+  def render("index.html", assigns) do
+    "rendering with assigns #{inspect(Map.keys(assigns))}"
+  end
 end
